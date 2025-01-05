af95d1b00b4e583aeb18e58b1ab113350006adf1
Asta1984 <salilmandal908@gmail.com>
2025-01-05 23:47:15 +0530

    timeline update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.unsplash.com","assets.aceternity.com","www.rgpv.ac.in"],
+    domains: ["images.unsplash.com","assets.aceternity.com","pub-bb773cad98e4488d80e5473dfc75aaef.r2.dev","www.rgpv.ac.in"],
   },
 };
 
--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -160,7 +160,7 @@ export function TimelineDemo() {
                 className="rounded-lg object-cover w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] h-20 md:h-44 lg:h-auto lg:aspect-[16/9]"
               />
               <Image
-                src="https://assets.aceternity.com/cards.png"
+                src="https://pub-bb773cad98e4488d80e5473dfc75aaef.r2.dev/Cf/5.jpg"
                 alt="cards template"
                 width={500}
                 height={500}
@@ -185,14 +185,14 @@ export function TimelineDemo() {
             </p>
             <div className="grid grid-cols-2 gap-4">
               <Image
-                src="https://assets.aceternity.com/pro/hero-sections.png"
+                src="https://pub-bb773cad98e4488d80e5473dfc75aaef.r2.dev/Cf/IMG-20230519-WA0019.jpg"
                 alt="hero template"
                 width={500}
                 height={500}
                 className="rounded-lg object-cover w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] h-20 md:h-44 lg:h-auto lg:aspect-[16/9]"
               />
               <Image
-                src="https://assets.aceternity.com/features-section.png"
+                src="https://pub-bb773cad98e4488d80e5473dfc75aaef.r2.dev/Cf/IMG-20220625-WA0000.jpg"
                 alt="feature template"
                 width={500}
                 height={500}
@@ -231,28 +231,28 @@ export function TimelineDemo() {
             </p>
             <div className="grid grid-cols-2 gap-4">
               <Image
-                src="https://assets.aceternity.com/pro/hero-sections.png"
+                src="https://pub-bb773cad98e4488d80e5473dfc75aaef.r2.dev/Cf/IMG-20230216-WA0000.jpg"
                 alt="hero template"
                 width={500}
                 height={500}
                 className="rounded-lg object-cover w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] h-20 md:h-44 lg:h-auto lg:aspect-[16/9]"
               />
               <Image
-                src="https://assets.aceternity.com/features-section.png"
+                src="https://pub-bb773cad98e4488d80e5473dfc75aaef.r2.dev/Cf/IMG-20240112-WA0014.jpg"
                 alt="feature template"
                 width={500}
                 height={500}
                 className="rounded-lg object-cover w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] h-20 md:h-44 lg:h-auto lg:aspect-[16/9]"
               />
               <Image
-                src="https://assets.aceternity.com/pro/bento-grids.png"
+                src="https://pub-bb773cad98e4488d80e5473dfc75aaef.r2.dev/Cf/Screenshot_20240122-210723_Video%20Player.png"
                 alt="bento template"
                 width={500}
                 height={500}
                 className="rounded-lg object-cover w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] h-20 md:h-44 lg:h-auto lg:aspect-[16/9]"
               />
               <Image
-                src="https://assets.aceternity.com/cards.png"
+                src="https://pub-bb773cad98e4488d80e5473dfc75aaef.r2.dev/Cf/1711430995590.jpeg"
                 alt="cards template"
                 width={500}
                 height={500}
