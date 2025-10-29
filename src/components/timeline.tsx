9ba48526961b1545acc31f59f3b0d65fb016cf48
Asta1984 <salilmandal908@gmail.com>
2025-10-29 21:47:22 +0530

    up

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -94,6 +94,30 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
 
 export function TimelineDemo() {
     const data = [
+       {
+        title: "2025",
+        content: (
+          <div>
+            <p className="text-white-400 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
+              <Image
+                src="/cybrom.png"
+                alt="cybrom Logo"
+                width={200}
+                height={100}
+              />Artificial Intelligence Trainer at Cybrom technology Pvt Ltd.
+            </p>
+            <div className="grid grid-cols-2 gap-4">
+              <ImageZoom
+                src="https://pub-d02e3aa7d09f4d5d9261e5d7e4bae228.r2.dev/knhs.jfif"
+                alt="ashish_c"
+                width={500}
+                height={500}
+                className="rounded-lg object-cover w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] h-20 md:h-44 lg:h-auto lg:aspect-[16/9]"
+              />
+            </div>
+          </div>
+        ),
+      },
       {
         title: "2025",
         content: (
@@ -170,7 +194,7 @@ export function TimelineDemo() {
           </div>
         ),
       },
-              {
+      {
         title: "2023",
         content: (
           <div>
