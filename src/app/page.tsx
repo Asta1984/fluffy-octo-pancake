42fba882380489cfc5415ed4130a40f32209d6cd
Salil Mandal <salilmandal908@gmail>
2026-08-15 00:46:06 +0530

    UI re

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,20 +9,21 @@ import { AnimatedText } from "@/components/AnimatedText";
 import Project_Card from "@/components/projects"
 import Link from "next/link"
 import { Navbar } from "@/components/ui/mini-navbar";
+import ScrollIndicator from "@/components/ScrollIndicator";
 
-const SocialLinks = ({ iconSize = "w-5 h-5", bgClass = "bg-transparent", hoverClass = "hover:bg-sky-950" }) => (
-  <div className="flex gap-2 mb-10 md:mx-52">
+const SocialLinks = ({ iconSize = "w-5 h-5", bgClass = "bg-transparent/30", hoverClass = "hover:bg-zinc-900/50" }) => (
+  <div className="flex gap-2 p-4 ">
     <Link href="mailto:salilmandal908@gmail.com" className={`p-2 rounded-md ${hoverClass} border ${bgClass} transition-colors duration-200`}>
-      <FaEnvelope className={`${iconSize} text-gray-300`} />
+      <FaEnvelope className={`${iconSize} text-accent`} />
     </Link>
     <Link href="https://github.com/Asta1984" className={`p-2 border rounded-md ${hoverClass} ${bgClass} transition-colors duration-200`}>
-      <FaGithub className={`${iconSize} text-gray-300`} />
+      <FaGithub className={`${iconSize} text-accent`} />
     </Link>
     <Link href="https://x.com/mandal_sal88300" className={`p-2 border rounded-md ${hoverClass} ${bgClass} transition-colors duration-200`}>
-      <FaXTwitter className={`${iconSize} text-gray-300`} />
+      <FaXTwitter className={`${iconSize} text-accent`} />
     </Link>
     <Link href="https://www.linkedin.com/in/salil-m-3a73b1138/" className={`p-2 border rounded-md ${hoverClass} ${bgClass} transition-colors duration-200`}>
-      <FaLinkedin className={`${iconSize} text-gray-300`} />
+      <FaLinkedin className={`${iconSize} text-accent`} />
     </Link>
   </div>
 );
@@ -30,30 +31,25 @@ const SocialLinks = ({ iconSize = "w-5 h-5", bgClass = "bg-transparent", hoverCl
 const ProfilePage = () => {
   return (
     <>
-      <Navbar />
-
+    <div className="py-12">
+      <Navbar/>
+    </div>
       {/* Hero Section */}
-      <div className="relative z-10">
+      {/* Main Content */}
+      <div className="min-h-screen p-6">
+        <RealisticLightspeedWarpDrive />
+      <div className="relative min-h-screen bg-black/20 backdrop-transparent-md border border-white/10 rounded-lg text-gray-200">
+      <div className="absolute">
         <Image 
           src="https://pub-d02e3aa7d09f4d5d9261e5d7e4bae228.r2.dev/endless.webp"
           alt="hero banner"
           width={1536}
           height={1024}
+          className="rounded-2xl"
         />
-        <h1 className="absolute inset-0 top-24 md:top-72 text-center text-3xl font-bold">
-          <span className="text-xl sm:text-5xl font-OnlinePrivileges md:mb-6 bg-clip-text bg-stone-600 md:mt-10">
-            Hi! This is Salil....
-          </span>
-        </h1>
-      </div>
-
-      {/* Main Content */}
-      <div className="min-h-screen p-6">
-        <RealisticLightspeedWarpDrive />
-
-        <div className="relative min-h-screen bg-black/20 backdrop-transparent-md border border-white/10 rounded-lg text-gray-200">
-          <div className="mx-auto py-16 px-7">
-            <div className="space-y-6">
+      </div>    
+          <div className="mx-auto px-7 z-30">
+            <div className="space-y-6 relative">
 
               {/* About Section */}
               <section id="about">
@@ -66,35 +62,27 @@ const ProfilePage = () => {
                   transition={{ duration: 1, ease: "easeOut" }}
                   exit={{ opacity: 0 }}
                 >
-                  <div className="md:w-4/5 space-y-4 tracking-wider font-Typewriter text-gray-300 text-xs bg-slate-800 bg-opacity-75 border md:mx-auto rounded-xl p-5 mt-6">
-                  <h1 className="text-4xl md:px-7 pt-10 font-OnlinePrivileges mb-8 text-white">
-                    Info
+                  <div className="md:w-4/5 space-y-4 tracking-wider font-Typewriter text-zinc-100 text-xs bg-slate-800 bg-slate-900/40 md:mx-auto rounded-xl md:mt-6 p-5 border-accent">
+                  <h1 className="text-4xl md:px-7 pt-10 font-OnlinePrivileges mb-8 text-zinc-100">
+                    Salil Mandal
                   </h1>
                     <p className="md:px-7">
-                      I&apos;m a software Engineer with hands-on experience in building scalable, secure,
-                      and innovative applications across diverse domains. My technical expertise spans
-                      Python, JavaScript, TypeScript, React, Node.js, FastAPI, and Django REST Framework,
-                      where I&apos;ve delivered high-impact solutions in AI, cloud-native systems, and Web3.
+                      I&apos;m a NERD in capital letters.diverse domains.
                     </p>
                     <p className="md:px-7">
-                      I&apos;ve worked on projects ranging from LiDAR-based vehicle classification
-                      systems to LLM-powered chatbots, healthcare platforms, crypto presale applications,
-                      and real-time attendance management systems. Along the way, I&apos;ve gained strong
-                      proficiency in AWS (Lambda, API Gateway, DynamoDB, EC2, S3), Docker, Kubernetes,
-                      Terraform, PostgreSQL, MongoDB, Redis, and Firebase, enabling me to architect
-                      end-to-end solutions with scalability in mind.
+                      I like to build, I have build and I will build
                     </p>
                     <p className="md:px-7">
-                      Beyond development, I enjoy experimenting with LLM APIs, AI/ML models,
-                      and automation tools like N8N, Zapier, and AI Agents to create smarter workflows.
+                      if you want me to larp, yap, build, vibecode etcetra
                       You could reach out to me via email.
                     </p>
                   </div>
                 </motion.div>
               </section>
 
+
               {/* Works Section */}
-              <section id="works">
+              <section id="works" >
                 <h2 className="text-4xl px-7 pt-10 font-OnlinePrivileges mt-10 mb-4 text-white">Projects Cooked</h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 md:p-4 gap-10">
                  <Project_Card />
